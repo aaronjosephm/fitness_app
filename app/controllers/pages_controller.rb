b92be292c3611bae8c34d80b6fe6f668b6bfc5fc
Aaron Mednick <aaronjosephm@gmail.com>
2018-09-08 14:36:08 -0400

    added other pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def landing
   end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def fitness
+  end
+
+  def mealplans
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#landing'
+
+  get '/about', to: 'pages#about'
+
+  get '/contact', to: 'pages#contact'
+
+  get '/fitness', to: 'pages#fitness'
+
+  get '/mealplans', to: 'pages#mealplans'
 end
